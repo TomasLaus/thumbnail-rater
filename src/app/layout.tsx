bf2f28392594533c6a518b14cb4222621cd28c51
Tomas Laus <tomaslaus99@gmail.com>
2024-06-06 20:05:12 -0300

    Theme toggler

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <Providers>
-      <html lang='en'>
+      <html lang='en' suppressHydrationWarning>
         <body className={inter.className}>{children}</body>
       </html>
     </Providers>
