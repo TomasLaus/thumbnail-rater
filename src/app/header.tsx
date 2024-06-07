f26551d5ca4e2f1978ee839aedfe6399af933ba2
Tomas Laus <tomaslaus99@gmail.com>
2024-06-07 20:29:18 -0300

    adding Header

---
+++ src/app/header.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import { ModeToggle } from './mode-toggle';
+import Link from 'next/link';
+
+export function Header() {
+  return (
+    <div className='border-b'>
+      <div className='h-16 container flex justify-between items-center'>
+        <Link href='/'>ThumbnailRater</Link>
+
+        <div className='flex gap-8'>
+          <SignedIn>
+            <Link href='/dashboard' className='link'>
+              Dashboard
+            </Link>
+            <Link href='/create' className='link'>
+              Create
+            </Link>
+            <Link href='/explore' className='link'>
+              Explore
+            </Link>
+          </SignedIn>
+          <SignedOut>
+            <Link href='/pricing' className='link'>
+              Pricing
+            </Link>
+            <Link href='/about' className='link'>
+              About
+            </Link>
+          </SignedOut>
+        </div>
+
+        <div className='flex gap-4 items-center'>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <ModeToggle />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      {isSignedIn ? <SignOutButton /> : <SignInButton />}
 
       {isSignedIn && (
         <form
