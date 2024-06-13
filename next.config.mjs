912376d7846f8d1dfd453b1f3f3828791b42a887
Tomas Laus <tomaslaus99@gmail.com>
2024-06-12 23:07:39 -0300

    Display Images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import { hostname } from 'os';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'fleet-wombat-27.convex.cloud'
+            }
+        ]
+    }
+};
 
 export default nextConfig;
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -6,10 +6,11 @@ import { UploadButton, UploadFileResponse } from '@xixixao/uploadstuff/react';
 import '@xixixao/uploadstuff/react/styles.css';
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function CreatePage() {
-  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
-//   const saveStorageId = useMutation(api.files.saveStorageId);
+    const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  const saveStorageId = useMutation(api.files.saveStorageId);
   const [imageA, setImageA] = useState('');
   const [imageB, setImageB] = useState('');
 
@@ -22,43 +23,58 @@ export default function CreatePage() {
         redesign or pick the best options.
       </p>
 
-
-        <form className='grid grid-cols-2 gap-8 mb-8'>
-          <div>
+      <form className='grid grid-cols-2 gap-8 mb-8'>
+        <div>
+          <div className='flex flex-col gap-4 rounded p-2'>
             <h2 className='text-2xl font-bold'>Test Image A</h2>
+
+            <Image
+              width={200}
+              height={200}
+              alt='test image a'
+              src={`https://fleet-wombat-27.convex.cloud/api/storage/${imageA}`}
+            />
+
             <UploadButton
               uploadUrl={generateUploadUrl}
               fileTypes={['image/*']}
-              onUploadComplete={
-                async (uploaded: UploadFileResponse[]) => {
-                    setImageA((uploaded[0].response as any).storageId );
-                  }
-              }
+              onUploadComplete={async (uploaded: UploadFileResponse[]) => {
+                setImageA((uploaded[0].response as any).storageId);
+              }}
               onUploadError={(error: unknown) => {
-                // Do something with the error.
                 alert(`ERROR! ${error}`);
               }}
             />
+          </div>
 
+          <div className='flex flex-col gap-4 rounded p-2'>
             <h2 className='text-2xl font-bold'>Test Image B</h2>
+
+            {imageB && (
+              <Image
+                width={200}
+                height={200}
+                alt='test image b'
+                src={`${process.env.NEXT_PUBLIC_CONVEX_URL}/api/storage/${imageB}`}
+              />
+            )}
 
             <UploadButton
               uploadUrl={generateUploadUrl}
               fileTypes={['image/*']}
-              onUploadComplete={
-                async (uploaded: UploadFileResponse[]) => {
-                    setImageB((uploaded[0].response as any).storageId );
-                  }
-              }
+              onUploadComplete={async (uploaded: UploadFileResponse[]) => {
+                setImageB((uploaded[0].response as any).storageId);
+              }}
               onUploadError={(error: unknown) => {
                 // Do something with the error.
                 alert(`ERROR! ${error}`);
               }}
             />
           </div>
+        </div>
 
-          <Button>Create Thumbnail Test</Button>
-        </form>
+        <Button>Create Thumbnail Test</Button>
+      </form>
     </div>
   );
 }
